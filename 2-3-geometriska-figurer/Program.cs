0ee174776d64fb222413bb8a31ea75afbae03aca
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-10-15 22:20:58 +0200

    Added headlines to the test

--- 2-3-geometriska-figurer/Program.cs
+++ 2-3-geometriska-figurer/Program.cs
@@ -27,6 +27,7 @@ namespace _2_3_geometriska_figurer
             Ellipse test2 = new Ellipse(1, 3);
             Ellipse test3 = new Ellipse(10, 300);
 
+            Console.WriteLine("{0}{1, 10:G3}{2, 10:G3}{3, 10:G3}{4, 10:G3}", "Figur".PadRight(10), "Längd", "Bredd", "Omkrets", "Area");
             Console.WriteLine(test.ToString());
             Console.WriteLine(test2.ToString());
             Console.WriteLine(test1.ToString());
